a2d01cb471da9687be59fc085350e9a9b274883d
bbiircan <bircanbircan1963@gmail.com>
2023-11-19 19:44:33 +0300

    Yazılım Geliştirici Yetiştirme Kampı 11.Gün

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -39,7 +39,7 @@ namespace Business.Concrete
             //iş kodları
             //yetkisi var mı? 
 
-            if (DateTime.Now.Hour==22)
+            if (DateTime.Now.Hour == 6)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
